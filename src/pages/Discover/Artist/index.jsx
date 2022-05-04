e3b1d0a01b1cbc0d59173c746bdf5f21f0b5d586
mufeng2333 <2441260435@qq.com>
2022-05-04 11:38:49 +0800

    添加歌手分类

--- src/pages/Discover/Artist/index.jsx
+++ src/pages/Discover/Artist/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Card,
   CardMedia,
@@ -8,17 +8,59 @@ import {
   Skeleton,
   Paper,
   Typography,
+  ToggleButton,
+  ToggleButtonGroup,
 } from "@mui/material";
 import { useRequest } from "ahooks";
 import { getToplistArtist } from "../../../api";
 import Artists from "../../../components/Artists";
+import Title from "../../../components/Title";
 
 const Artist = () => {
-  const { loading: loadingToplistArtist, data: toplistArtist } =
-    useRequest(getToplistArtist);
+  const [type, setType] = React.useState(1);
+
+  const types = [
+    {
+      type: 1,
+      name: "华语",
+    },
+    {
+      type: 2,
+      name: "欧美",
+    },
+    {
+      type: 3,
+      name: "韩国",
+    },
+    {
+      type: 4,
+      name: "日本",
+    },
+  ];
+
+  const { loading: loadingToplistArtist, data: toplistArtist } = useRequest(
+    () => getToplistArtist(type),
+    {
+      refreshDeps: [type],
+    }
+  );
 
   return (
     <>
+      <Title title="热门歌手" />
+      <ToggleButtonGroup
+        color="primary"
+        value={type}
+        exclusive
+        onChange={(_, newType) => setType(newType)}
+        sx={{ my: 1 }}
+      >
+        {types.map((t) => (
+          <ToggleButton value={t.type} key={t.type}>
+            {t.name}
+          </ToggleButton>
+        ))}
+      </ToggleButtonGroup>
       {loadingToplistArtist ? (
         <>
           <Grid spacing={2} container>
